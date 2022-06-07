efd03ee1fe3306272cbfc376025be775972daac1
Eyob Eshetu <eyob2901@gmail.com>
2022-06-07 12:37:40 -0700

    I play what I call the alphabet game

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,1 @@
-#include <stdio.h>
-/**
- * main- prints _putchar
- * Return: always 0
- */
-int main(void)
-{
-        printf("_putchar\n");
-                return (0);
-}
+void print_numbers(void);}
