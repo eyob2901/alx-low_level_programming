88de3a7d8679bfda320e9b1c1e624cbf0111af74
Eyob Eshetu <eyob2901@gmail.com>
2022-06-07 14:30:37 -0700

    check lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 
 /**
  * _islower - checks for lowercase character
+ * @c: the character to check
  * Returns 1 if c is lowercase
  * Returns 0 otherwide
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c<= 'z');
+	return (c >= 'a' && c <= 'z');
 }
