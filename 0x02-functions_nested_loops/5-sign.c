3db53536f9dddfc831b2ad3a66e5cb8be5f46d6e
Eyob Eshetu <eyob2901@gmail.com>
2022-06-07 15:36:30 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -19,6 +20,6 @@ int print_sign(int n)
 	} else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
 	}
+		return (-1);
 }
