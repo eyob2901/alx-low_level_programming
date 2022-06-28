ad58423f6ce48938587f7924760c0d3b1afb14f7
Eyob Eshetu <eyob2901@gmail.com>
2022-06-28 05:28:00 -0700

    sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: storaged char
+ * Return: a pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *chr;
+unsigned int i;
+
+if (size == 0)
+return (NULL);
+chr = malloc(sizeof(c) * size);
+
+if (chr == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+chr[i] = c;
+return (chr);
+}
